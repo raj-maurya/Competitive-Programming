a966f4c4f9b1b5b74bcfb58946005508ed263294
HUNTER...!!! <rajkmaurya111@gmail.com>
2015-11-02 01:29:33 +0530

    Create Utopian.py

---
+++ Utopian.py
@@ -0,0 +1,13 @@
+import sys
+T = int(sys.stdin.readline())
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    height = 1
+        
+    for i in range(N):
+        if i % 2 == 0:
+            height *= 2
+        else:
+            height += 1
+            
+    print(height)
