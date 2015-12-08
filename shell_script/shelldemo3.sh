6bdf2c85da446a01b6eb45d23103b2517d0aa10a
Raj Maurya <rajkmaurya111@gmail.com>
2015-12-08 20:25:42 +0530

    Bash/shell script

---
+++ shell_script/shelldemo3.sh
@@ -0,0 +1,9 @@
+clear
+
+#scrpt that demos,command line arguments
+
+echo " the total number of  command line are $#"
+echo " $0 the script name"
+echo  " $1 the first name argumrnt "
+echo " $2 the second argument"
+echo " all of them are : $*  or $@"
