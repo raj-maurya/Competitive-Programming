3da63e907956728996ff4cb30525877f3b435d94
Raj Maurya <rajkmaurya111@gmail.com>
2015-12-15 22:08:10 +0530

    test programme

---
+++ Random programmes/final.py
@@ -0,0 +1,67 @@
+str = "hello there bob"
+print str[0]
+
+def fred():
+   print "Zap"
+
+def jane():
+   print "ABC"
+
+fred()
+jane()
+fred()
+
+
+print "hello world"
+
+lst = []
+lst.append(4)
+lst.append(10)
+lst.append(21)
+lst.append(6)
+print lst[2]
+
+def hello():
+    print "Hello" 
+    print "There"
+
+x = 10
+x = x + 1
+
+x = 0
+for value in [3, 41, 12, 9, 74, 15] :
+    if value < 10 :
+        x = x + value 
+print x
+
+tot = 0
+for i in range(5):
+    tot = tot + i
+print tot
+
+def hello():
+    print "Hello"
+    print "Fun"
+ 
+hello()
+hello()
+
+stx = "hello there bob how are you"
+wds = stx.split()
+print wds[2]
+
+abc = None
+for val in lst:
+    if abc == None or val > abc:
+        abc = val
+
+print abc
+
+
+
+
+x , y = 3, 4
+
+
+
+
