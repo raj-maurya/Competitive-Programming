15339c1a25603a92a7faae051c4d6ba48593dd59
Raj Maurya <rajkmaurya111@gmail.com>
2016-06-27 20:37:59 +0530

    Rosalind solutions

---
+++ Random programmes/ini3.py
@@ -0,0 +1,8 @@
+a= 'vapBSKLjBxo3NvwfHahc2gvR49heBombyxyirregularisqRrhlKKBydSrusMLznUy0CYUd0DBIEwoOtcqV3TAux7eWlTZ8HGdGZU5OJDRZ9qRfYc7Dplxal8G5Ohl0ZOqiAzohqgPNKlMsGwIr6yjuQ0viPHdX3JmRzAT0keLqc1iD.'
+w=28
+x=34
+y=35
+z=46
+c=a[w:x]
+d=a[y:z]
+print c+" "+d
---
+++ Random programmes/ini4.py
@@ -0,0 +1,8 @@
+a=4869
+b=8872
+c=0
+for i in range(a,b):
+    if(i%2==1):
+        c=c+i
+
+print c
