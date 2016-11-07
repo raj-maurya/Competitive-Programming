32f93dc198bc24f053837e1e9b076e7ce9fb981b
Raj Maurya <rajkmaurya111@gmail.com>
2016-11-08 03:48:57 +0530

    Cracking_the_code_solution

---
+++ Hackerrank/cracking_the_code_challenge_solution/ctci-array-left-rotation.cpp
@@ -0,0 +1,46 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+        cin >> a[a_i];
+    }
+    
+    int x,y,z=0;
+    z=k%n;
+    for(x=z; x<n; x++){
+        cout<<a[x]<<" ";
+    }
+    for(y=0; y<z; y++){
+        cout<<a[y]<<" ";
+    }
+
+    return 0;
+}
